a4213ea48a84075d6288952ff6cbaffd2bd62578
johrmohr <jormor64@gmail.com>
2021-06-21 20:48:14 -0700

    Signed-off-by: johrmohr <jormor64@gmail.com>

---
+++ Class/README.md
@@ -0,0 +1,2 @@
+# MorenoJordan_CIS17A_45398
+CIS 17A Lehr RCC Summer 2021
---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,34 @@
+/*
+    Author: Jordan Moreno
+    Date:   21 June 2021, 8:41 PM
+    Purpose: Hello World Program for GitHub
+ */
+
+//System Libraries
+#include <iostream>    //Input/Output Library
+using namespace std;   //Library Name-space
+
+//User Libraries
+
+//Global/Universal Constants -- No Global Variables
+//Science, Math, Conversions, Higher Dimensioned constants only
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set the Random number seed
+    
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Process, map inputs to outputs
+    
+    //Display your initial conditions as well as outputs.
+    cout<<"Hello World";
+    
+    //Exit stage right
+    return 0;
+}
+
